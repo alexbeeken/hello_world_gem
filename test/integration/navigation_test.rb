efa850499b97251ac5ad855b90b5a71c5162bef8
Alex Beeken <aeb242@gmail.com>
2018-07-03 11:14:38 -0700

    fix any filename issues and add test

--- test/integration/navigation_test.rb
+++ test/integration/navigation_test.rb
@@ -1,8 +1,16 @@
 require 'test_helper'
 
 class NavigationTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'hello_world route mounts correctly' do
+    get '/hello_world'
+    
+    assert_response :success
+  end
+
+  test 'hello_world displays correct message' do
+    get '/hello_world'
+    
+    assert_select '.hello-world', 'Hello World!'
+  end
 end
 
